a4396619939ba77307eac1b13d0985aed27ae93f
Maklen Estrada <162079502+maklenestrada@users.noreply.github.com>
2024-09-23 20:55:54 -0400

    Added function to calculate inverse T matrix

--- MatrixMath.cpp
+++ MatrixMath.cpp
@@ -22,3 +22,20 @@ void MatrixMath::MatrixMult(double ans[4][4],double T_L[4][4],double T_R[4][4])
         }
     }
 }
+
+//Inverse of T Matrix
+void InverseTMatrix(double ans[4][4],double T[4][4])
+{
+    // Upper left 3x3 of result is transpose of upper left 3x3 of tran
+    for (int i=0 ; i<3 ; ++i)
+        for (int j =0 ; j<3 ; ++j)
+            ans[i][j] = T[j][i] ;
+    // Set the values for the last column of the result
+    ans[3][0] = ans[3][1] = ans[3][2] = 0.0 ;
+    ans[3][3] = 1.0 ;
+    // Initialize the values of the last column of the result.
+    ans[0][3] = ans[1][3] = ans[2][3] = 0.0 ;
+    for(int i = 0; i < 3; i++)
+        for(int j = 0; j < 3; j++)
+            ans[i][3] -= ans[i][j] * T[j][3] ;
+}
--- MatrixMath.h
+++ MatrixMath.h
@@ -10,5 +10,8 @@ public:
 
     //Function for matrix-matrix multiplication (T (4x4) times T (4x4))
     void MatrixMult(double ans[4][4],double T_L[4][4],double T_R[4][4]);
+
+    //Function to calculate the inverse of a T (4x4) Matrix
+    void InverseTMatrix(double ans[4][4],double T[4][4]);
 };
 #endif //ROBOT_MANIPULATOR__MATRIXMATH_H
